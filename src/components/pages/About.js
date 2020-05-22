07330e6e7e662bfe3490187156db57b677d2f839
Odunmorayo Mayowa <mayowajohn533@gmail.com>
2020-05-23 00:06:21 +0100

    adding author

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
+
 export default function about() {
     return (
         <React.Fragment>
             <h1 style={{ textAlign: 'center', color: '#ff4500'}}>About</h1>
             <p style={myStyle}>This is a random Project-CheckList app v1.0.0.</p><hr></hr><br />
             <div style={myStyle2}>This application helps the user to create a Todo list. You can delete the custom todo items and create a new one.</div>
+            <div style={myStyle3}>
+                <p>This project was created by</p>
+                <hr />
+                <p>Odunmorayo Mayowa</p>
+            </div>
         </React.Fragment>
     )
 }
@@ -22,3 +28,11 @@ const myStyle2 = {
     fontSize: '20px', 
     color: '#fff'
 }
+
+const myStyle3 = {
+    textAlign: 'center',
+    color: '#fff',
+    marginTop: '30vh',
+    fontSize: '20px'
+}
+
